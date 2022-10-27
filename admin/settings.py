e2843d56f450120e29d607dc5278df3b527bf271
jacksonhuish88 <jacksonhuish88@gmail.com>
2022-10-27 13:26:22 -0600

    1

--- admin/settings.py
+++ admin/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'GainsPages.contrib.GainpagesConfig',
+    'GainPages.apps.GainpagesConfig',
 ]
 
 MIDDLEWARE = [
