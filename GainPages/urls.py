ffebd5086faafaa6c043e3e4920ca8eb69d36aa2
Ezra DeMaere <96100231+edemaere@users.noreply.github.com>
2022-11-14 14:34:53 -0700

    Update urls.py

--- GainPages/urls.py
+++ GainPages/urls.py
@@ -4,9 +4,9 @@ from .views import aboutPageView, indexPageView, calculatorView, liftTypeView, b
 
 urlpatterns = [
     path("about/", aboutPageView, name="about"),
-    path('', indexPageView, name='index'),
     path("calculators/", calculatorView, name="calculators"),
     path("lifts/", liftTypeView, name="lifts"),
     path("bodyTypes/", bodyTypeView, name="bodyTypes"),
     path("crudTable/", crudTableView, name="crudTable"),
+    path('', indexPageView, name='index'),
 ]
